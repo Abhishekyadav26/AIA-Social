77ceb9ca8d10f3dbd245d75539096d1a2d80c28d
Abhishekyadav26 <aky732244@gmail.com>
2024-11-02 15:16:20 +0530

    initial commit

---
+++ src/app/client.ts
@@ -0,0 +1,5 @@
+import { createThirdwebClient } from "thirdweb";
+
+const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+
+export const client = createThirdwebClient({ clientId: clientId as string });
